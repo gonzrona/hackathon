9663525d55921a1d1ea75ac5c95a310433e44938
Matthew Nicely <mnicely@nvidia.com>
2021-05-07 11:53:10 -0400

    Cleaned up headers

--- 2D_Program/solver_functions/DST.c
+++ 2D_Program/solver_functions/DST.c
@@ -1,33 +1,8 @@
 #include "../headers/structs.h"
 
 #include <cufftw.h>
-#include <nvToolsExt.h>
+#include "cuda_helper.h"
 
-//*****************************************************
-//                        NVTX                         
-//*****************************************************
-#include<stdint.h>
-static const uint32_t colors[] = { 0xff00ff00, 0xff0000ff, 0xffffff00, 0xffff00ff, 0xff00ffff, 0xffff0000, 0xffffffff };
-static const int num_colors = sizeof(colors)/sizeof(uint32_t);
-
-#define PUSH_RANGE(name,cid) { \
-    int color_id = cid; \
-    color_id = color_id%num_colors;\
-    nvtxEventAttributes_t eventAttrib = {0}; \
-    eventAttrib.version = NVTX_VERSION; \
-    eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE; \
-    eventAttrib.colorType = NVTX_COLOR_ARGB; \
-    eventAttrib.color = colors[color_id]; \
-    eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII; \
-    eventAttrib.message.ascii = name; \
-    nvtxRangePushEx(&eventAttrib); \
-}
-#define POP_RANGE nvtxRangePop();
-//*****************************************************
-//                        NVTX                         
-//*****************************************************
-
-// void DST(DSTN dst, double _Complex *b, double _Complex *bhat, fftw_plan plan, double *in, fftw_complex *out) {
 void forwardDST(System sys, DSTN dst, double _Complex *rhs, double _Complex *rhat, fftw_plan plan, double *in, fftw_complex *out, fftw_plan plan2, double *in2, fftw_complex *out2) {
  
     int i,j,my;
