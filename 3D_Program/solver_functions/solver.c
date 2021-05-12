4ddbc4bee1caf7121f32934230e9a3d9a0d90a68
Ron Gonzales <gonzrona@login1.ascent.olcf.ornl.gov>
2021-05-12 18:30:57 -0400

    added what I believe is needed in the solver

--- 3D_Program/solver_functions/solver.c
+++ 3D_Program/solver_functions/solver.c
@@ -1,10 +1,127 @@
 #include "../headers/structs.h"
 #include "../headers/prototypes.h"
 
+
+#include <assert.h>
+#include <string.h>
+
+#include <cuda_runtime.h>
+#include <cuComplex.h>
+//#include <cuComplex.h>
+//#include <cufftw.h>
+
+
 void DST(int Nx, int Ny, double *b_2D, double *bhat, fftw_plan p1,  double *in1, fftw_complex *out1, fftw_plan p2,  double *in2, fftw_complex *out2);
 
 #ifdef USE_CUFFTW
 
+
+void solver(System sys) {
+
+
+    int i, num_streams = 4;
+    cudaStream_t streams[num_streams];
+    for ( i = 0; i < num_streams; i++ ) {
+        cudaStreamCreateWithFlags( &streams[i], cudaStreamNonBlocking );
+    }
+
+    int Nx = sys.lat.Nx, Ny = sys.lat.Ny;
+    
+    
+    int NR = 2*Nx + 2;
+    int NC = NR/2 + 1;
+    
+    size_t size_in  = sizeof( double ) * NR * Ny;
+    size_t size_out = sizeof( fftw_complex ) * NC * Ny;
+        
+    double *in, *in2;
+    fftw_complex *out, *out2;
+    cudaMallocManaged( ( void ** )&in, size_in, 1 );
+    cudaMallocManaged( ( void ** )&out, size_out, 1 );
+
+    cudaMemPrefetchAsync( in, size_in, 0, streams[0] );
+    cudaMemPrefetchAsync( out, size_out, 0, streams[1] );
+
+    cudaMemsetAsync( in, size_in, 0, streams[0] );
+    cudaMemsetAsync( out, size_out, 0, streams[1] );
+    
+    
+    NR = 2*Ny + 2;
+    NC = NR/2 + 1;
+    size_in  = sizeof( double ) * NR * Nx;
+    size_out = sizeof( fftw_complex ) * NC * Nx;
+    
+    cudaMallocManaged( ( void ** )&in2, size_in, 1 );
+    cudaMallocManaged( ( void ** )&out2, size_out, 1 );
+    
+    cudaMemPrefetchAsync( in2, size_in, 0, streams[0] );
+    cudaMemPrefetchAsync( out2, size_out, 0, streams[1] );
+    
+    cudaMemsetAsync( in2, size_in, 0, streams[0] );
+    cudaMemsetAsync( out2, size_out, 0, streams[1] );
+    
+    
+    cuDoubleComplex *d_y;
+    cudaMalloc( ( void ** )( &d_y ), sys.lat.Nxyz * sizeof( cuDoubleComplex ) );
+    
+    
+    cudaMemPrefetchAsync( sys.rhs, sys.lat.Nxyz * sizeof( double _Complex ), 0, streams[0] ) ;
+
+    cudaMemPrefetchAsync( sys.U, sys.lat.Nxyz * sizeof( double _Complex ), 0, streams[2] ) ;
+    cudaMemPrefetchAsync( sys.L, sys.lat.Nxyz * sizeof( double _Complex ), 0, streams[2] ) ;
+    cudaMemPrefetchAsync( sys.Up, sys.lat.Nxyz * sizeof( double _Complex ), 0, streams[2] ) ;
+
+    cudaMemPrefetchAsync( sys.sol, sys.lat.Nxyz * sizeof( double _Complex ), 0, streams[3] ) ;
+
+    /**********************BATCHED***************************/
+    NR = 2*Nx + 2;
+    NC = NR/2 + 1;
+    const int rank = 1;
+    int howmany = Ny;
+    int n[] = {NR};
+    const int istride = 1; const int ostride = 1;
+    int idist = NR; int odist = NC;
+    int *inembed = NULL, *onembed = NULL;
+    /**********************BATCHED***************************/
+    
+    fftw_plan plan, plan2;
+    
+    plan = fftw_plan_many_dft_r2c( rank, n, howmany, in, inembed, istride, idist, out, onembed, ostride, odist, FFTW_ESTIMATE );
+    
+    
+    
+    /**********************BATCHED***************************/
+    NR = 2*Ny + 2;
+    NC = NR/2 + 1;
+    const int rank2 = 1;
+    int howmany2 = Nx;
+    int n2[] = {NR};
+    const int istride2 = 1; const int ostride2 = 1;
+    int idist2 = NR; int odist2 = NC;
+    int *inembed2 = NULL, *onembed2 = NULL;
+    /**********************BATCHED***************************/
+    
+    
+    plan2 = fftw_plan_many_dft_r2c( rank2, n2, howmany2, in2, inembed2, istride2, idist2, out2, onembed2, ostride2, odist2, FFTW_ESTIMATE );
+    
+    
+    cudaMemPrefetchAsync( sys.sol, sys.lat.Nxyz * sizeof( double _Complex ), cudaCpuDeviceId, NULL );
+
+    
+    cudaFree( in );
+    cudaFree( out );
+    cudaFree( in2 );
+    cudaFree( out2 );
+
+    
+    for (i = 0; i < num_streams; i++ ) {
+        cudaStreamDestroy( streams[i] );
+    }
+    
+    
+    fftw_destroy_plan( plan );  /********************* FFTW *********************/
+    fftw_destroy_plan( plan2 ); /********************* FFTW *********************/
+}
 #else
 void solver(System sys) {
     int Nx = sys.lat.Nx, Ny = sys.lat.Ny, Nz = sys.lat.Nz;
